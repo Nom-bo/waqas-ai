24838a1929b485aedddbf78363a2abf25463b4d4
Nom-bo <no.aslam28@gmail.com>
2024-09-04 21:52:54 -0700

    final-ass

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const page = () => {
     return (
         <div><figure className="bg-cyan-100 rounded-xl p-8 dark:bg-slate-800">
-        <Image className="w-24 h-24 rounded-full mx-auto" src="/address.jfif" alt="" width="384" height="512"/>
+        <Image className="w-24 h-24 rounded-full mx-auto" src="/address.jpg" alt="" width="384" height="512"/>
         <div className="pt-6 text-center space-y-4">
           <blockquote>
             <p className="text-lg font-medium">
